167b6fafaf1a0d04f2da26676ae9d5f6f89dd8ed
gwr0-0 <win101wd@hotmail.com>
2017-09-23 03:35:36 +0800

    用户模块开发

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,29 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+/**
+ * Created by gwr0-0 on 2017/9/20.
+ */
+public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetRestPassword(String username, String passwordNew, String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+
+    ServerResponse<User> updateUserInformation(User user);
+
+    ServerResponse<User> getUserInformation(Integer userId);
+
+}
