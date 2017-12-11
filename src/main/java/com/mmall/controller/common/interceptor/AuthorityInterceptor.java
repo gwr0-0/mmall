f6fb2f99e964a7b3e2e77a14592da6d77ab3fc88
gwr0-0 <win101wd@hotmail.com>
2017-12-11 20:26:52 +0800

    富文本上传拦截器拦截

--- src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
+++ src/main/java/com/mmall/controller/common/interceptor/AuthorityInterceptor.java
@@ -1,5 +1,6 @@
 package com.mmall.controller.common.interceptor;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
@@ -28,7 +29,6 @@ import java.util.Map;
  *（1）preHandle: 在执行controller处理之前执行，返回值为boolean ,返回值为true时接着执行postHandle和afterCompletion，如果我们返回false则中断执行
  *（2）postHandle:在执行controller的处理后，在ModelAndView处理前执行
  *（3）afterCompletion ：在DispatchServlet执行完ModelAndView之后执行
- 源码如下
  */
 @Slf4j
 public class AuthorityInterceptor implements HandlerInterceptor {
@@ -41,6 +41,12 @@ public class AuthorityInterceptor implements HandlerInterceptor {
         //解析HandlerMethod
         String methodName = handlerMethod.getMethod().getName();
         String className = handlerMethod.getBean().getClass().getSimpleName();
+
+        //拦截特定请求直接返回true，效果等同于 mvc:exclude-mapping
+        if (StringUtils.equals(className, "UserManageController") && StringUtils.equals(methodName, "login")) {
+            log.info("拦截器拦截到管理员登陆请求，，className:{}, methodName:{}", className, methodName);
+            return true;
+        }
 
         //解析参数，具体的参数key以及value是什么
         StringBuffer requestParamBuffer = new StringBuffer();
@@ -59,6 +65,8 @@ public class AuthorityInterceptor implements HandlerInterceptor {
             requestParamBuffer.append(mapKey).append("=").append(mapValue);
         }
 
+        log.info("权限拦截器拦截到请求,className:{}, methodName:{}, param:{}", className, methodName, requestParamBuffer);
+
         User user = null;
         String loginToken = CookieUtil.readLoginToken(request);
         if (StringUtils.isNotEmpty(loginToken)) {
@@ -67,6 +75,7 @@ public class AuthorityInterceptor implements HandlerInterceptor {
         }
 
         //返回false，即不会调用controller中的方法
+        //上传由于富文本的控件要求，要特殊处理返回值，这里面区分是否登录以及是否有权限
         if (user == null || (user.getRole().intValue() != Const.Role.ROLE_ADMIN)) {
             response.reset();
             response.setCharacterEncoding("UTF-8");
@@ -74,9 +83,23 @@ public class AuthorityInterceptor implements HandlerInterceptor {
 
             PrintWriter printWriter = response.getWriter();
             if (user == null) {
-                printWriter.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，用户未登陆")));
+                if(StringUtils.equals(className,"ProductManageController") && (StringUtils.equals(methodName,"richtextImgUpload") )){
+                    Map resultMap = Maps.newHashMap();
+                    resultMap.put("success",false);
+                    resultMap.put("msg","用户未登录，请登录管理员");
+                    printWriter.print(JsonUtil.obj2String(resultMap));
+                }else{
+                    printWriter.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截,用户未登录")));
+                }
             } else {
-                printWriter.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，无权限登陆")));
+                if(StringUtils.equals(className,"ProductManageController") && (StringUtils.equals(methodName,"richtextImgUpload") )){
+                    Map resultMap = Maps.newHashMap();
+                    resultMap.put("success",false);
+                    resultMap.put("msg","无权限操作");
+                    printWriter.print(JsonUtil.obj2String(resultMap));
+                }else{
+                    printWriter.print(JsonUtil.obj2String(ServerResponse.createByErrorMessage("拦截器拦截，无权限登陆")));
+                }
             }
             printWriter.flush();
             printWriter.close();
